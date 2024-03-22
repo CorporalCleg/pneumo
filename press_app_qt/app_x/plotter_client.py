122788ab24793816e2b9a35beb31921e39588e4f
CorporalCleg <swampinhabitant@icloud.com>
2024-03-22 13:26:58 -0400

    Ok client <-> server

--- press_app_qt/app_x/plotter_client.py
+++ press_app_qt/app_x/plotter_client.py
@@ -1,27 +1,68 @@
 import socket
-import time
-import random
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from icecream import ic
 
 
-class plotter_client:
-    def __init__(self):
+class p_client():
+    def __init__(self, window_size=10):
         self.port = 1414
         self.hostname = 'localhost'
         self.server_addr = (self.hostname, self.port)
-        
 
-    def send(self, measure="0.0", target="0.0"):
+        self.window_size = window_size
+        self.time = [0]
+        self.targets = [0.0]
+        self.data = [0.0]
+        
+    
+    def recv_n_redraw(self, frame):
 
         #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client = socket.socket()
-        print(self.hostname)
+        #ic(self.hostname)
         client.connect(self.server_addr)
-        print(f'message = {message}')
-        message = (measure + ' ' + target).encode()
-        client.send(message)
+
+        #get telemetry from server
+        d = client.recv(1024)
+        ic(d)
         client.close()
 
-if __name__ == "__main__":
-    clt = plotter_client()
-    while True:
-        clt.send()
+        measure, target = d.decode().split()
+    
+        if len(self.time) > self.window_size:
+
+            #update values
+            self.data = self.data[-self.window_size:] + [float(measure)]
+            self.targets = self.targets[-self.window_size:] + [float(target)]
+            self.time = self.time[-self.window_size:] + [self.time[-1] + 1]
+
+            #render
+            axes.cla()
+            axes.set_title('Pressure')
+            axes.set_ylim(-3, 3)
+            axes.set_xlim(self.time[-self.window_size], self.time[-1])
+            axes.plot(self.time, self.data, color="red", label='measurements')
+            axes.plot(self.time, self.targets, color="blue", label='target')
+            axes.legend(loc=1)
+            
+        else:
+            axes.plot(self.time, self.data, color="red", label='measurements')
+            axes.plot(self.time, self.targets, color="blue", label='target')
+            self.data.append(float(measure))
+            self.targets.append(float(target))
+            self.time.append(self.time[-1] + 1)
+
+    
+
+if __name__ == '__main__':
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(10, 5))
+
+
+    plt.style.use("ggplot")
+    plt.ylim(-2, 2)
+
+    clnt = p_client()
+    anim = FuncAnimation(fig, lambda frame: clnt.recv_n_redraw(frame), frames = None, interval=50)
+
+    plt.show()
--- press_app_qt/app_x/plotter_server.py
+++ press_app_qt/app_x/plotter_server.py
@@ -1,69 +1,28 @@
 import socket
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from icecream import ic
-
-
-class pserver():
-    def __init__(self, window_size=10):
+import random
+import time
+class p_server:
+    def __init__(self):
         self.server = socket.socket()
-        self.hostname = '0.0.0.0'
-        print(self.hostname)
         self.port = 1414
+        self.hostname = '0.0.0.0'
+
         self.server.bind((self.hostname, self.port))
         self.server.listen(5)
 
-        self.window_size = window_size
-        self.time = [0]
-        self.targets = [0.0]
-        self.data = [0.0]
-        
-        
-        
-    
-    def recv_n_redraw(self, frame):
 
-        #get telemetry from server
+    def send(self, measure="0.0", target="0.0"):
+
+        measure = str(random.random())
+        print(measure)
+        message = (measure + ' ' + target).encode()
+
         self.conn, _ = self.server.accept()
-        d = self.conn.recv(1024)
+        self.conn.send(message)
         self.conn.close()
-        
-        measure, target = d.decode().split()
-    
-        if len(self.time) > self.window_size:
 
-            #update values
-            self.data = self.data[-self.window_size:] + [float(measure)]
-            self.targets = self.targets[-self.window_size:] + [float(target)]
-            self.time = self.time[-self.window_size:] + [self.time[-1] + 1]
-
-            #render
-            axes.cla()
-            axes.set_title('Pressure')
-            axes.set_ylim(-3, 3)
-            axes.set_xlim(self.time[-self.window_size], self.time[-1])
-            axes.plot(self.time, self.data, color="red", label='measurements')
-            axes.plot(self.time, self.targets, color="blue", label='target')
-            axes.legend(loc=1)
-
-            
-        else:
-            axes.plot(self.time, self.data, color="red", label='measurements')
-            axes.plot(self.time, self.targets, color="blue", label='target')
-            self.data.append(float(measure))
-            self.targets.append(float(target))
-            self.time.append(self.time[-1] + 1)
-
-    
-
-if __name__ == '__main__':
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(10, 5))
-
-
-    plt.style.use("ggplot")
-    plt.ylim(-2, 2)
-
-    s = pserver()
-    anim = FuncAnimation(fig, lambda frame: s.recv_n_redraw(frame), frames = None, interval=500)
-
-    plt.show()
+if __name__ == "__main__":
+    srvr = p_server()
+    while True:
+        time.sleep(0.5)
+        srvr.send()
